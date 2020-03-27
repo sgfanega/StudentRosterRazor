7822eb8d9a520350238751af0c46f0a6e404d06f
sgfanega <steve.fanega.ii@gmail.com>
2020-03-27 13:24:49 -0500

    Added Student Model

---
+++ StudentRosterRazor/Model/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRosterRazor.Model
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Grade { get; set; }
+        public string Gender { get; set; }
+    }
+}
