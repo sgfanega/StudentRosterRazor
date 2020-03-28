f19c2653ba409522ff429b482155ee94bd8b4d65
sgfanega <steve.fanega.ii@gmail.com>
2020-03-27 23:13:51 -0500

    Added JS for Table

---
+++ StudentRosterRazor/Pages/StudentRoster/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentRosterRazor.Model;
+
+namespace StudentRosterRazor.Pages.StudentRoster
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Student> Students { get; set; }
+
+        public async Task OnGet()
+        {
+            Students = await _db.Student.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int studentID)
+        {
+            var Student = await _db.Student.FindAsync(studentID);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            _db.Student.Remove(Student);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ StudentRosterRazor/wwwroot/js/StudentRoster.js
@@ -0,0 +1,42 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#student_roster').DataTable({
+        "ajax": {
+            "url": "/api/student",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "studentid", "width": "15%" },
+            { "data": "firstname", "wdith": "15%" },
+            { "data": "middlename", "wdith": "15%" },
+            { "data": "lastname", "wdith": "15%" },
+            { "data": "grade", "wdith": "15%" },
+            { "data": "gender", "wdith": "15%" },
+            {
+                "data": "studentid",
+                "render": function (data) {
+                    return `
+                    <div class="text-center">
+                        <a href="/Index/Edit?=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                        Edit
+                        </a>
+                        &nbsp;
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;' onclick=Delete('api/student?id='+${data})>
+                        Delete
+                        </a>
+                    </div>`;
+                }, "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No Data Found"
+        },
+        "width": "100%"
+    });
+}
