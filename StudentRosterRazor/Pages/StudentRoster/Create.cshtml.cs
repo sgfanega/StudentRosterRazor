80c200fa216cfd5d698fb47efc60ab8f112d9d59
sgfanega <steve.fanega.ii@gmail.com>
2020-03-28 00:10:26 -0500

    Add Create Student and Validations

---
+++ StudentRosterRazor/Pages/StudentRoster/Create.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentRosterRazor.Model;
+
+namespace StudentRosterRazor.Pages.StudentRoster
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Student.AddAsync(Student);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
