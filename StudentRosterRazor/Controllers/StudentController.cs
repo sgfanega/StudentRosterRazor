8cea452dc57bf8509f5b168f94e205d7898add66
sgfanega <steve.fanega.ii@gmail.com>
2020-03-28 19:42:42 -0500

    Added Controller folder and Migration

---
+++ StudentRosterRazor/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentRosterRazor.Model;
+
+namespace StudentRosterRazor.Controllers
+{
+    [Route("api/Student")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StudentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Student.ToList() } );
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int ID)
+        {
+            var studentFromDb = await _db.Student.FirstOrDefaultAsync(b => b.ID == ID);
+            if (studentFromDb == null)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Error While Deleting"
+                });
+            }
+            _db.Student.Remove(studentFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new
+            {
+                success = true,
+                message = "Delete Completed"
+            });
+        }
+    }
+}
---
+++ StudentRosterRazor/Migrations/20200328183902_AddStudentRosterToDb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentRosterRazor.Migrations
+{
+    public partial class AddStudentRosterToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentID = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    MiddleName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: false),
+                    Grade = table.Column<string>(nullable: false),
+                    Gender = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
