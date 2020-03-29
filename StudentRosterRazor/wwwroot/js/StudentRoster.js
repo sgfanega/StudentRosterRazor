9520e1699621bddd84910ef728e175853a17e83c
sgfanega <steve.fanega.ii@gmail.com>
2020-03-28 19:42:24 -0500

    Added Delete for JS

--- StudentRosterRazor/wwwroot/js/StudentRoster.js
+++ StudentRosterRazor/wwwroot/js/StudentRoster.js
@@ -12,31 +12,56 @@ function loadDataTable() {
             "datatype": "json"
         },
         "columns": [
-            { "data": "studentid", "width": "15%" },
-            { "data": "firstname", "wdith": "15%" },
-            { "data": "middlename", "wdith": "15%" },
-            { "data": "lastname", "wdith": "15%" },
-            { "data": "grade", "wdith": "15%" },
-            { "data": "gender", "wdith": "15%" },
+            { "data": "studentID", "width": "20%" },
+            { "data": "firstName", "wdith": "20%" },
+            { "data": "middleName", "wdith": "20%" },
+            { "data": "lastName", "wdith": "20%" },
+            { "data": "grade", "wdith": "20%" },
+            { "data": "gender", "wdith": "20%" },
             {
-                "data": "studentid",
+                "data": "id",
                 "render": function (data) {
                     return `
                     <div class="text-center">
-                        <a href="/Index/Edit?=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                        <a href="/StudentRoster/Edit?=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
                         Edit
                         </a>
                         &nbsp;
-                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;' onclick=Delete('api/student?id='+${data})>
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;' onclick=Delete('/api/student?id='+${data})>
                         Delete
                         </a>
                     </div>`;
-                }, "width": "40%"
+                }, "width": "20%"
             }
         ],
         "language": {
             "emptyTable": "No Data Found"
         },
         "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
     });
 }
